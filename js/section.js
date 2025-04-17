b6529b1b96744de7a4bdd879e6afad79b082e039
lucassantosdahora <116045229+lucassantosdahora@users.noreply.github.com>
2025-04-16 21:40:21 -0300

    ajustes em estilos na section-4

--- js/section.js
+++ js/section.js
@@ -2,7 +2,8 @@ function observeSectionsFade() {
   const section1 = document.getElementById("section-1");
   const section2 = document.getElementById("section-2");
   const section3 = document.getElementById("section-3");
-  const sections = [section1, section2,section3];
+  const section4 = document.getElementById("section-4");
+  const sections = [section1, section2,section3,section4];
   const isSmallScreen = window.innerWidth <= 768;
   const thresholdValue = isSmallScreen ? 0.3 : 0.6;
   const observer = new IntersectionObserver((entries, observer) => {
