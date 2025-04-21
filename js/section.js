c6b9a80f76a3f0008f537926b71c76fcb448908a
lucassantosdahora <116045229+lucassantosdahora@users.noreply.github.com>
2025-04-21 11:35:26 -0300

    criando animação de scrolll e links na section de dê o start

--- js/section.js
+++ js/section.js
@@ -3,7 +3,8 @@ function observeSectionsFade() {
   const section2 = document.getElementById("section-2");
   const section3 = document.getElementById("section-3");
   const section4 = document.getElementById("section-4");
-  const sections = [section1, section2,section3,section4];
+  const section5 = document.getElementById("section-5");
+  const sections = [section1, section2,section3,section4,section5];
   const isSmallScreen = window.innerWidth <= 768;
   const thresholdValue = isSmallScreen ? 0.3 : 0.5;
   const observer = new IntersectionObserver((entries, observer) => {
