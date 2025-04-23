0d344e33ef12deccba9ffd95c6ca6e20dd85d835
lucassantosdahora <116045229+lucassantosdahora@users.noreply.github.com>
2025-04-22 22:19:30 -0300

    criando animação de navbar fixa

---
+++ js/navbarColor.js
@@ -0,0 +1,17 @@
+navbar =document.querySelector(".box header");
+const isSmallScreen = window.innerWidth < 1024;
+const largeScreen = window.innerWidth >= 2560;
+function navbarColor() {
+    if (scrollY >= 200) {
+        navbar.style.backgroundColor = isSmallScreen ? '#174c7260' : '#174c7260';
+        navbar.style.height = isSmallScreen ? "270px" : '100px';
+        navbar.style.height = largeScreen ? "270px" : '100px';
+        navbar.style.transition = '0.5s'
+    }
+    else{
+        navbar.style.backgroundColor = '';
+        navbar.style.height = isSmallScreen ? "270px" : '150px';
+        navbar.style.transition = '0.5s'
+    }
+};
+window.addEventListener('scroll', navbarColor);
