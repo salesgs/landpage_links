49d6ff85518c92f77070eca611ff044a296ae988
lucassantosdahora <116045229+lucassantosdahora@users.noreply.github.com>
2025-04-22 22:28:52 -0300

    ajutes sidebar no mobile

--- js/navbarColor.js
+++ js/navbarColor.js
@@ -4,13 +4,13 @@ const largeScreen = window.innerWidth >= 2560;
 function navbarColor() {
     if (scrollY >= 200) {
         navbar.style.backgroundColor = isSmallScreen ? '#174c7260' : '#174c7260';
-        navbar.style.height = isSmallScreen ? "270px" : '100px';
-        navbar.style.height = largeScreen ? "270px" : '100px';
+        navbar.style.height = isSmallScreen ? "270px" : largeScreen ? "270px" : '100px';
+        // navbar.style.height = largeScreen ? "270px" : '100px';
         navbar.style.transition = '0.5s'
     }
     else{
         navbar.style.backgroundColor = '';
-        navbar.style.height = isSmallScreen ? "270px" : '150px';
+        navbar.style.height = isSmallScreen ? "200px" : '150px';
         navbar.style.transition = '0.5s'
     }
 };
